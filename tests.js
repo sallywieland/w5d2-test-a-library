c6082e32f11c028ac5b587eea14c870f74b211d8
Sally Wieland <sallybwieland@gmail.com>
2016-10-26 15:55:05 -0400

    third commit with changes made from class today

--- tests.js
+++ tests.js
@@ -9,13 +9,11 @@ describe('MoDom', function() {
   })
   describe('md.one()', function() {
     it('should find one matching element using CSS selector', function() {
-      md.one('span')
       expect(md.one('span').innerHTML).to.equal('Toggle navigation')
     })
   })
   describe('md.all()', function() {
     it('should find all matching elements using any CSS selector', function() {
-      md.all('div')
       expect(md.all('div').length).to.equal(14)
     })
   })
@@ -27,8 +25,8 @@ describe('MoDom', function() {
   })
   describe('md.addClass()', function() {
     it('should add a CSS class', function() {
-      md.addClass('h4', 'red')
-      expect(md.one('h4').classList.contains('red')).to.be.true
+      md.addClass('h4', 'red') // set, add, modification --> test it here --> inserting it in/doing something
+      expect(md.one('h4').classList.contains('red')).to.be.true // making sure the insert/doing something is actually happening --> think of as parent double checking something
     })
   })
   describe('md.removeClass()', function() {
@@ -37,22 +35,31 @@ describe('MoDom', function() {
       expect(md.one('span').classList.contains('sr-only')).to.be.false
     })
   })
+
   describe('md.hasClass()', function() {
     it('should return true if an element contains a CSS class in it\'s CSS attribute', function() {
-      md.hasClass('div', 'container-fluid')
-      expect(md.one('div').classList.contains('container-fluid')).to.be.true
+      expect(md.hasClass('div', 'container-fluid')).to.be.true // we are actually expecting an answer back --> not a change in modification
     })
   })
+  // describe('md.getAttr()', function() {
+  //   it('should get an HTML attribute from an element', function() {
+  //     md.getAttr('input', 'placeholder')
+  //     expect(md.one('input').placeholder).to.equal('Search...')
+  //   })
+  // })
   describe('md.getAttr()', function() {
     it('should get an HTML attribute from an element', function() {
-      md.getAttr('input', 'placeholder')
-      expect(md.one('input').placeholder).to.equal('Search...')
+      expect(md.getAttr('input', 'placeholder')).to.equal('Search...') // again, getting something in return, which is why we only have one line.
     })
   })
   describe('md.setAttr()', function() {
     it('should set an HTML attribute on an element', function() {
-      md.setAttr('input', 'placeholder', 'SEARCH')
-      expect(md.one('input').placeholder).to.equal('SEARCH')
+      md.setAttr('input', 'placeholder', 'SEARCH') // actually changing/modifying/setting something
+      // expect(md.one('input').placeholder).to.equal('SEARCH')
+      expect(md.getAttr('input', 'placeholder')).to.equal('SEARCH')
+      // expect(document.querySelector('input').getAttribute('placeholder').to.equal('SEARCH')) // use if you don't want to depend on your other tests
+      // MOCKING: setting up conditions and then testing so the mock you created actually works
+      md.setAttr('input', 'placeholder', 'Search...') // flipping test back (undoing) after you test it
     })
   })
   describe('md.setHTML()', function() {
@@ -63,8 +70,7 @@ describe('MoDom', function() {
   })
   describe('md.getHTML()', function() {
     it('should get the innerHTML of an element', function() {
-      md.getHTML('.navbar-brand')
-      expect(md.one('.navbar-brand').innerHTML).to.equal('hello')
+      expect(md.getHTML('.navbar-brand')).to.equal('hello')
     })
   })
   describe('md.flipPage()', function() {
